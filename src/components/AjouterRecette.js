29feecf265e9c8106044f2c2bba2751742d5ac4e
MaksymY <maksym.yankivskyypro@gmail.com>
2019-03-27 12:57:38 +0100

    formular add

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 class AjouterRecette extends Component {
+    state = {
+        nom: '',
+        image: '',
+        ingredients:'',
+        instructions:''
+    }
+
     render () {
         return (
-          <div>
-              <h2>Ajouter Recette</h2>
+          <div className="card">
+            <form className="admin-form ajouter-recette">
+              <input className="nom" type="text" />
+              <input className="image" type="text" />
+              <textarea name="ingredients" cols="30" row="3" ></textarea>
+              <textarea name="instructions" cols="30" row="15" ></textarea>
+            </form>
           </div>
         )
     }
